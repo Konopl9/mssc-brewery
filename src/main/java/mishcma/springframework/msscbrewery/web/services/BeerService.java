64462f02692a2f3ba2e32011a6a57e85f8ca59ed
Maksym_Mishchenko <maksimus2998@gmail.com>
2022-08-18 09:12:31 +0200

    added beer dto, controller and service

--- src/main/java/mishcma/springframework/msscbrewery/web/services/BeerService.java
+++ src/main/java/mishcma/springframework/msscbrewery/web/services/BeerService.java
@@ -1,2 +1,9 @@
-package mishcma.springframework.msscbrewery.web.services;public interface BeerService {
+package mishcma.springframework.msscbrewery.web.services;
+
+import mishcma.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
 }
--- src/main/java/mishcma/springframework/msscbrewery/web/services/BeerServiceImpl.java
+++ src/main/java/mishcma/springframework/msscbrewery/web/services/BeerServiceImpl.java
@@ -1,2 +1,12 @@
-package mishcma.springframework.msscbrewery.web.services;public class BeerServiceImpl {
+package mishcma.springframework.msscbrewery.web.services;
+
+import mishcma.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID()).beerName("Galaxy Cat").beerStyle("Pale Ale").build();
+    }
 }
