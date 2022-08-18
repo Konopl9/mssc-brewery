b3bdb55cad365c3992a3b0cc90c5feee34bd9396
Maksym_Mishchenko <maksimus2998@gmail.com>
2022-08-18 10:20:46 +0200

    added POST, PUT, DELETE mapping for beerDto

--- src/main/java/mishcma/springframework/msscbrewery/web/services/BeerService.java
+++ src/main/java/mishcma/springframework/msscbrewery/web/services/BeerService.java
@@ -1,9 +1,17 @@
 package mishcma.springframework.msscbrewery.web.services;
 
 import mishcma.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public interface BeerService {
     BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deleteById(UUID beerId);
 }
--- src/main/java/mishcma/springframework/msscbrewery/web/services/BeerServiceImpl.java
+++ src/main/java/mishcma/springframework/msscbrewery/web/services/BeerServiceImpl.java
@@ -1,12 +1,31 @@
 package mishcma.springframework.msscbrewery.web.services;
 
+import lombok.extern.slf4j.Slf4j;
 import mishcma.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
         return BeerDto.builder().id(UUID.randomUUID()).beerName("Galaxy Cat").beerStyle("Pale Ale").build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // Todo implement
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+      log.debug("Deleting a beer...");
     }
 }
